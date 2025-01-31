302a07b8aaebaa1ba9f89949f00176cc2f0fe6a8
chrisj0110 <45946825+chrisj0110@users.noreply.github.com>
2025-01-31 17:23:23 -0600

    BREAKING CHANGE: don't support various splits anymore, let the user handle that

--- README.md
+++ README.md
@@ -2,11 +2,7 @@
 
 ## Purpose
 
-Slide in a buffer to take notes
-
-## Examples:
-
-![examples](demo/demo.gif)
+Slide in a window to take notes
 
 ## How To Use
 
@@ -17,37 +13,32 @@ return {
     "chrisj0110/notes-slider",
     config = function()
         require('notes-slider').setup({
-            horizontal_split_size = 15,  -- if omitted, default is 15
-            vertical_split_size = 70,  -- default is 70
             scratch_file_prefix = "scratch-", -- default is "scratch-"
             scratch_file_extension = "md", -- default is "txt"
             scratch_file_dir = "~/bin", -- default is $HOME
         })
 
-        -- if you want to use the current tmux session name in the file name, call toggle_scratch_using_tmux_name()
-        -- first param is whether you want a horizontal split or a vertical split
-        -- second param is whether you want the notes buffer "after" the current one (i.e. "right"/"below" the current one, depending on horizontal vs vertical)
-        vim.api.nvim_set_keymap('n', '[s', ':lua require("notes-slider").toggle_scratch_using_tmux_name(false, false)<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', ']s', ':lua require("notes-slider").toggle_scratch_using_tmux_name(true, true)<CR>', { noremap = true, silent = true })
-        -- or if you want to just open the file in the current buffer, use toggle_open_scratch_using_tmux_name(). If open it will toggle back to previous buffer
-        vim.api.nvim_set_keymap('n', '-s', ':lua require("notes-slider").toggle_open_scratch_using_tmux_name()<CR>', { noremap = true, silent = true })
+        -- how you split windows is up to you. Below is how I have mine setup.
 
-        -- or if you don't want to automatically use the tmux name, just call toggle_scratch()
-        -- first two params are the same as above
-        -- third param is the text to put in the file name instead of the tmux session name
-        vim.api.nvim_set_keymap('n', '[s', ':lua require("notes-slider").toggle_scratch(false, false, "abc")<CR>', { noremap = true, silent = true })
-        vim.api.nvim_set_keymap('n', ']s', ':lua require("notes-slider").toggle_scratch(true, true, "abc")<CR>', { noremap = true, silent = true })
-        -- or if you want to just open the file in the current buffer, use toggle_open_scratch_using_tmux_name(). If open it will toggle back to previous buffer
-        vim.api.nvim_set_keymap('n', '-s', ':lua require("notes-slider").toggle_open_scratch("abc")<CR>', { noremap = true, silent = true })
+        vim.opt.equalalways = false -- Disable automatic resizing of splits
+
+        -- if you want to use the current tmux session name in the file name, call open_notes_using_tmux_session_name():
+
+        -- horizontal split on top:
+        vim.api.nvim_set_keymap('n', '[s', ':aboveleft split | wincmd K | resize 15<CR>:lua require("notes-slider").open_notes_using_tmux_session_name()<CR>', { noremap = true, silent = true })
+
+        -- vertical split on right:
+        vim.api.nvim_set_keymap('n', ']s', ':vsplit | wincmd R | vertical resize 70<CR>:lua require("notes-slider").open_notes_using_tmux_session_name()<CR>', { noremap = true, silent = true })
+
+        -- open in current window:
+        vim.api.nvim_set_keymap('n', '-s', ':lua require("notes-slider").open_notes_using_tmux_session_name()<CR>', { noremap = true, silent = true })
+
+        -- or if you don't want to automatically use the tmux name, pass in your own part of the file name (see setup() above for the rest of the file path)
+        -- vim.api.nvim_set_keymap('n', '[s', ':aboveleft split | wincmd K | resize 15<CR>:lua require("notes-slider").open_notes("abc")<CR>', { noremap = true, silent = true })
+        -- vim.api.nvim_set_keymap('n', ']s', ':vsplit | wincmd R | vertical resize 70<CR>:lua require("notes-slider").open_notes("abc")<CR>', { noremap = true, silent = true })
+        -- vim.api.nvim_set_keymap('n', '-s', ':lua require("notes-slider").open_notes("abc")<CR>', { noremap = true, silent = true })
     end
 }
 ```
 
-Or you can call functions directly, for example:
-
-```lua
-:lua require("notes-slider").toggle_scratch(false, false, "abc")
-```
-
-Calling the same function/mapping again toggles it open/closed.
-
+Note: in earlier versions this supported splitting windows in a variety of ways, but now I'm leaving that for you to configure, and this plugin just focuses on opening the proper file.
--- lua/notes-slider/init.lua
+++ lua/notes-slider/init.lua
@@ -1,17 +1,9 @@
 ---@class Config
----@field tag string|nil custom tag to use
----@field auto_save boolean|nil whether to save the buffer once tweaking it
----@field horizontal_split_size integer|nil how tall the horizontal notes buffer is
----@field vertical_split_size integer|nil how wide the vertical notes buffer is
 ---@field scratch_file_prefix string|nil default notes file prefix
 ---@field scratch_file_extension string|nil default notes file extension
 ---@field scratch_file_dir string|nil default dir to save notes file
 local M = {}
 
----@type integer
-local horizontal_split_size = 15
----@type integer
-local vertical_split_size = 70
 ---@type string
 local scratch_file_prefix = 'scratch-'
 ---@type string
@@ -22,13 +14,6 @@ local scratch_file_dir = os.getenv("HOME") or "~"
 ---Set values based on user's configuration
 ---@param config Config
 function M.setup(config)
-    if config and config.horizontal_split_size then
-        horizontal_split_size = config.horizontal_split_size
-    end
-    if config and config.vertical_split_size then
-        vertical_split_size = config.vertical_split_size
-    end
-
     if config and config.scratch_file_prefix then
         scratch_file_prefix = config.scratch_file_prefix
     end
@@ -43,10 +28,6 @@ function M.setup(config)
 end
 
 vim = vim
-
--- Save cursor position per buffer
----@type table<integer, { [1]: integer, [2]: integer}>
-local cursor_positions = {}
 
 ---get name of the current tmux session, or empty string
 ---@return string
@@ -66,157 +47,20 @@ local function get_tmux_session_name()
     return session_name
 end
 
----toggle the window open/closed with the file name generated from tmux session name
----@param vertical boolean whether it's a vertical split or horizontal split
----@param after boolean whether the notes buffer will come below/right of the current buffer
-function M.toggle_scratch_using_tmux_name(vertical, after)
-    M.toggle_scratch(vertical, after, get_tmux_session_name())
-end
-
----toggle open/closed in the current the window with the file name generated from tmux session name
-function M.toggle_open_scratch_using_tmux_name()
-    M.toggle_open_scratch(get_tmux_session_name())
-end
-
----open a vertical split
----@param file_or_buf string file name or buffer number (string)
----@param is_buf boolean whether file_or_buf is a buffer or not
----@param after boolean whether the notes buffer will come below/right of the current buffer
----@param split_size integer the size of the window to open the notes buffer in
-local function open_vertical_split(file_or_buf, is_buf, after, split_size)
-    ---@type boolean
-    local original_splitright = vim.o.splitright
-    if after then
-        vim.cmd('set splitright')
-    else
-        vim.cmd('set nosplitright')
-    end
-
-    if is_buf then
-        vim.cmd('vsplit | buffer' .. file_or_buf)
-    else
-        vim.cmd('vsplit ' .. file_or_buf)
-    end
-
-    if after and not original_splitright then
-        -- if after=true and it was nosplitright then set it to nosplitright
-        vim.cmd('set nosplitright')
-    elseif not after and original_splitright then
-        -- if after=false and it was splitright then set it to splitright
-        vim.cmd('set splitright')
-    end
-
-    vim.cmd('vertical resize ' .. split_size)
-end
-
----open a horizontal split
----@param file_or_buf string file name or buffer number (string)
----@param is_buf boolean whether file_or_buf is a buffer or not
----@param after boolean whether the notes buffer will come below/right of the current buffer
----@param split_size integer the size of the window to open the notes buffer in
-local function open_horizontal_split(file_or_buf, is_buf, after, split_size)
-    ---@type boolean
-    local original_splitbelow = vim.o.splitbelow
-    if after then
-        vim.cmd('set splitbelow')
-    else
-        vim.cmd('set nosplitbelow')
-    end
-
-    if is_buf then
-        vim.cmd('split | buffer ' .. file_or_buf)
-    else
-        vim.cmd('split ' .. file_or_buf)
-    end
-
-    if after and not original_splitbelow then
-        -- if after=true and it was nosplitbelow then set it to nosplitbelow
-        vim.cmd('set nosplitbelow')
-    elseif not after and original_splitbelow then
-        -- if after=false and it was splitbelow then set it to splitbelow
-        vim.cmd('set splitbelow')
-    end
-
-    vim.cmd('resize ' .. split_size)
-end
-
 ---@param scratch_file_name string inner part of the file name - can be tmux session name, or custom value
 local function get_scratch_file(scratch_file_name)
     return scratch_file_dir .. "/" .. scratch_file_prefix .. scratch_file_name .. "." .. scratch_file_extension
 end
 
----toggle open/closed the notes window
----@param vertical boolean whether it's a vertical split or horizontal split
----@param after boolean whether the notes buffer will come below/right of the current buffer
----@param scratch_file_name string inner part of the file name - can be tmux session name, or custom value
-function M.toggle_scratch(vertical, after, scratch_file_name)
-    local scratch_file = get_scratch_file(scratch_file_name)
-
-    ---@type integer
-    local buf = vim.fn.bufnr(scratch_file)
-    if buf == -1 then
-        if vertical then
-            open_vertical_split(scratch_file, false, after, vertical_split_size)
-        else
-            open_horizontal_split(scratch_file, false, after, horizontal_split_size)
-        end
-    else
-        -- Save cursor position before closing the buffer
-        if vim.api.nvim_buf_is_loaded(buf) then
-            cursor_positions[buf] = vim.api.nvim_win_get_cursor(0)
-        end
-
-        -- Close the window containing the scratch file if it's open
-        ---@type integer[]
-        local wins = vim.api.nvim_list_wins()
-        for _, win in ipairs(wins) do
-            if vim.api.nvim_win_get_buf(win) == buf then
-                if vim.api.nvim_buf_get_option(buf, "modified") then
-                    local choice = vim.fn.input("Buffer has unsaved changes - (s)ave, (d)iscard, (c)ancel: ")
-                    if choice == "s" then
-                        vim.cmd('write')
-                        vim.cmd('bdelete ' .. buf)
-                    elseif choice == "d" then
-                        vim.cmd('bdelete! ' .. buf)
-                    end
-                else
-                    vim.cmd('bdelete ' .. buf)
-                end
-                return
-            end
-        end
-        -- if the buffer exists but not in a window, open in a new split
-        if vertical then
-            open_vertical_split(tostring(buf), true, after, vertical_split_size)
-        else
-            open_horizontal_split(tostring(buf), true, after, horizontal_split_size)
-        end
-    end
-
-    -- Restore cursor position if it was saved
-    if vim.api.nvim_buf_is_loaded(buf) and cursor_positions[buf] then
-        if cursor_positions[buf][1] > vim.api.nvim_buf_line_count(buf) then
-            -- it wants to go to a line that is greater than the number of lines in the buffer, so go to the last line
-            vim.api.nvim_win_set_cursor(0, {vim.api.nvim_buf_line_count(buf), 0})
-        else
-            vim.api.nvim_win_set_cursor(0, cursor_positions[buf])
-        end
-    end
+---open in the current the window the file name generated from tmux session name
+function M.open_notes_using_tmux_session_name()
+    M.open_notes(get_tmux_session_name())
 end
 
----toggle open/closed the notes file in the current window
+---open the notes file in the current window
 ---@param scratch_file_name string inner part of the file name - can be tmux session name, or custom value
-function M.toggle_open_scratch(scratch_file_name)
-    local scratch_file = get_scratch_file(scratch_file_name)
-
-    local current_buf = vim.api.nvim_get_current_buf()
-    local current_file = vim.api.nvim_buf_get_name(current_buf)
-
-    if current_file == scratch_file then
-        vim.cmd('b#')
-    else
-        vim.cmd('e ' .. scratch_file)
-    end
+function M.open_notes(scratch_file_name)
+    vim.cmd('e ' .. get_scratch_file(scratch_file_name))
 end
 
 return M
